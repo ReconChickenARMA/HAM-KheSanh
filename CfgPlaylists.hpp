8436b8956cbfa125428e1f876255f26c43a0ff49
OverlordZorn <dennisAzorn@gmail.com>
2025-03-13 00:03:05 +0100

    playlists

---
+++ CfgPlaylists.hpp
@@ -0,0 +1,15 @@
+class CfgPlaylists
+{
+    class ham_kheSanh_placeholder
+    {
+        name = "placeholder";
+        dependencies[] = {};
+        tracks[] = {
+            "cvo_music_chorniVoron",
+            "AmbientTrack01_F",
+            "AmbientTrack01a_F",
+            "AmbientTrack01b_F",
+            "OST_09_RF"
+        };
+    };
+};
