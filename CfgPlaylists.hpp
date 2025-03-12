5b4710c793c4551b64e547fcc11b34ccbb5047e7
OverlordZorn <dennisAzorn@gmail.com>
2025-03-13 00:16:15 +0100

    Update CfgPlaylists.hpp

--- CfgPlaylists.hpp
+++ CfgPlaylists.hpp
@@ -1,15 +1,71 @@
 class CfgPlaylists
 {
-    class ham_kheSanh_placeholder
+    class ham_kheSanh_Tense
     {
-        name = "placeholder";
-        dependencies[] = {};
+        name = "VN Tense";
+        dependencies[] = {"music_f_vietnam"};
         tracks[] = {
-            "cvo_music_chorniVoron",
-            "AmbientTrack01_F",
-            "AmbientTrack01a_F",
-            "AmbientTrack01b_F",
-            "OST_09_RF"
+            "vn_cover_blown",
+            "vn_prepare_for_battle",
+            "vn_prairie_fire",
+            "vn_calm_before_the_storm",
+            "vn_the_village",
+            "vn_behind_enemy_lines",
+            "vn_deadly_jungle",
+            "vn_enemy_territory",
+            "vn_stealth",
+            "vn_shadows_of_the_forest",
+            "vn_in_neptunes_realm",
+            "vn_jungle_boots"
+        };
+    };
+    class ham_kheSanh_Combat
+    {
+        name = "VN Combat";
+        dependencies[] = {"music_f_vietnam"};
+        tracks[] = {
+            "vn_contact",
+            "vn_time_to_leave",
+            "vn_imminent_attack",
+            "vn_trippin",
+            "vn_drafted",
+            "vn_there_it_is",
+            "vn_fire_in_the_sky",
+            "vn_route9",
+            "vn_voodoo_girl",
+            "vn_tequila_highway",
+            "vn_dont_cry_baby"
+        };
+    };
+    class ham_kheSanh_Travelling
+    {
+        name = "VN Travel/Release";
+        dependencies[] = {"music_f_vietnam"};
+        tracks[] = {
+            "vn_tour_of_duty",
+            "vn_prairie_fire",
+            "vn_prayer_of_the_fallen",
+            "vn_the_village",
+            "vn_im_home",
+            "vn_another_life",
+            "vn_unsung_heroes",
+            "vn_blues_for_suzy",
+            "vn_tequila_highway",
+            "vn_kitty_bar_blues",
+            "vn_freedom_bird",
+            "vn_up_here_looking_down"
+        };
+    };
+    class ham_kheSanh_Bad_Vibes
+    {
+        name = "VN Bad_Vibes";
+        dependencies[] = {"music_f_vietnam"};
+        tracks[] = {
+            "vn_death_scene",
+            "vn_hell_on_earth",
+            "Fallout",
+            "Defcon",
+            "Wasteland"
         };
     };
 };
